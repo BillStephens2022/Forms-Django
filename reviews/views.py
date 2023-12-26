9f6f5bc2fc6987642abd116ec8111c584aa4ab8b
BillStephens2022 <two4onebill@yahoo.com>
2023-12-26 13:31:18 -0500

    added ReviewForm class and rendered in reviews template, added form validation controls

--- reviews/views.py
+++ reviews/views.py
@@ -1,19 +1,24 @@
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
 
+from .forms import ReviewForm
+
 # Create your views here.
+
 
 def review(request):
     if request.method == "POST":
-        entered_username = request.POST["username"]
-        if entered_username == "":
-            return render(request, "reviews/review.html", {
-                "has_error": True
-            })
-        return HttpResponseRedirect("/thank-you")
+        form = ReviewForm(request.POST)
+        if form.is_valid():
+            print(form.cleaned_data)
+            return HttpResponseRedirect("/thank-you")
+    else:
+        form = ReviewForm()
+
     return render(request, "reviews/review.html", {
-        "has_error": False
+        "form": form
     })
+
 
 def thank_you(request):
     return render(request, "reviews/thank_you.html")
