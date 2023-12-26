a358d8a2489727f5625d85f1dc91f42539257d79
BillStephens2022 <two4onebill@yahoo.com>
2023-12-26 16:53:36 -0500

    fixed syntax error

--- reviews/urls.py
+++ reviews/urls.py
@@ -3,6 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-  path("", views.review),
+  path("", views.ReviewView.as_view()),  # updated for class based approach
+  # path("", views.review),  # removed once started using class based approach
   path("thank-you", views.thank_you)
 ]
