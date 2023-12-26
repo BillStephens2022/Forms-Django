5cbfb67f443b9b4bb72463e8853d188be9ee2e63
BillStephens2022 <two4onebill@yahoo.com>
2023-12-26 16:39:25 -0500

    updated review function to save the form since we are now using a ModelForm

--- reviews/views.py
+++ reviews/views.py
@@ -2,7 +2,7 @@ from django.http import HttpResponseRedirect
 from django.shortcuts import render
 
 from .forms import ReviewForm
-from .models import Review
+# from .models import Review
 
 # Create your views here.
 
@@ -11,11 +11,13 @@ def review(request):
     if request.method == "POST":
         form = ReviewForm(request.POST)
         if form.is_valid():
-            review = Review(
-                user_name=form.cleaned_data['user_name'], 
-                review_text=form.cleaned_data['review_text'], 
-                rating=form.cleaned_data['rating'])
-            review.save()
+             # removed code block below after replacing manual form with a ModelForm
+            # review = Review( 
+            #     user_name=form.cleaned_data['user_name'], 
+            #     review_text=form.cleaned_data['review_text'], 
+            #     rating=form.cleaned_data['rating'])
+            # review.save()
+            form.save()
             return HttpResponseRedirect("/thank-you")
     else:
         form = ReviewForm()
