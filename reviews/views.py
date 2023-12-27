98b403cfab960dfb8690d57599e3384565c0fdce
BillStephens2022 <two4onebill@yahoo.com>
2023-12-27 09:30:28 -0500

    replaced View with FormView for the review form page

--- reviews/views.py
+++ reviews/views.py
@@ -5,25 +5,28 @@ from django.shortcuts import render
 from django.views import View
 from django.views.generic.base import TemplateView
 from django.views.generic import ListView, DetailView
+from django.views.generic.edit import FormView
 from .forms import ReviewForm
 from .models import Review
 
 # Create your views here.
-class ReviewView(View):
-    def get(self, request):
-        form = ReviewForm()
-        return render(request, "reviews/review.html", {
-            "form": form
-        })
+class ReviewView(FormView):
+    form_class = ReviewForm
+    template_name = "reviews/review.html"
+    success_url = "/thank-you"
 
-    def post(self, request):
-        form = ReviewForm(request.POST)
-        if form.is_valid():
-            form.save()
-            return HttpResponseRedirect("/thank-you")
-        return render(request, "reviews/review.html", {
-            "form": form
-        })
+    def form_valid(self, form):
+        form.save()
+        return super().form_valid(form)
+
+    # def post(self, request):
+    #     form = ReviewForm(request.POST)
+    #     if form.is_valid():
+    #         form.save()
+    #         return HttpResponseRedirect("/thank-you")
+    #     return render(request, "reviews/review.html", {
+    #         "form": form
+    #     })
 
 # removed this code block once implementing a class based view approach
 # def review(request):
@@ -50,7 +53,7 @@ class ThankYouView(TemplateView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context["message"] = "This works!"
+        context["message"] = "Your review has been submitted!"
         return context
     
 
@@ -79,4 +82,3 @@ class ReviewsListView(ListView):
 class SingleReviewView(DetailView):
     template_name = "reviews/single_review.html"
     model = Review
-    
